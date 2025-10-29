efa5615d4081c247faec2fcddad40c3b2c977588
patricklafrance <pa.lafrance@gmail.com>
2025-10-29 16:16:47 -0400

    chore: Migrate to ESLint 9

---
+++ eslint.config.ts
@@ -0,0 +1,11 @@
+import { defineCoreConfig, definePackageJsonConfig, defineYamlConfig, } from "@workleap/eslint-configs";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+    globalIgnores([
+        "docs"
+    ]),
+    defineCoreConfig(),
+    definePackageJsonConfig(),
+    defineYamlConfig()
+]);
